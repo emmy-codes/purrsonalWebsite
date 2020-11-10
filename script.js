2741f34d2e7f92f2493db85d71c8f27bf75a8534
emmy_codes <alunia.moon@gmail.com>
2020-11-10 18:03:10 +0100

    wip

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let openStartMenu = document.querySelector(".start-button");
 function openMenu() {
     if (startMenu.classList.contains('hidden')) {
         startMenu.classList.remove('hidden')
+        openStartMenu.classList.add('clicked-start-button');
     } else {
         startMenu.classList.add('hidden')
+        openStartMenu.classList.remove('clicked-start-button');
     };
 }
 openStartMenu.addEventListener("click", openMenu);
